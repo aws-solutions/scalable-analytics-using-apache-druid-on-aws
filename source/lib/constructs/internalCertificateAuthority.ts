40a14461665d2bacb38df2b8988ae7a5dca82ade
Van Vo Thanh <vovan@amazon.com>
2024-03-05 11:37:19 +1100

    fix: retain tls secret and kms key on update

--- source/lib/constructs/internalCertificateAuthority.ts
+++ source/lib/constructs/internalCertificateAuthority.ts
@@ -17,44 +17,50 @@
 
 import * as cdk from 'aws-cdk-lib';
 import * as cr from 'aws-cdk-lib/custom-resources';
+import * as kms from 'aws-cdk-lib/aws-kms';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as kms from 'aws-cdk-lib/aws-kms';
 import * as path from 'path';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 import { Construct } from 'constructs';
 
 export class InternalCertificateAuthority extends Construct {
-    public readonly TlsCertificate: secretsmanager.ISecret;
+  public readonly TlsCertificate: secretsmanager.ISecret;
 
-    public constructor(scope: Construct, id: string) {
-        super(scope, id);
+  public constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-        this.TlsCertificate = new secretsmanager.Secret(this, 'tls-certificate', {
-            description: 'TLS certificates for druid internal components',
-            encryptionKey: new kms.Key(this, 'tls-certificate-encryption-key', {
-                enableKeyRotation: true,
-                removalPolicy: cdk.RemovalPolicy.DESTROY,
-            }),
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
-        });
+    this.TlsCertificate = new secretsmanager.Secret(this, 'tls-certificate', {
+      description: 'TLS certificates for druid internal components',
+      encryptionKey: new kms.Key(this, 'tls-certificate-encryption-key', {
+        enableKeyRotation: true,
+        removalPolicy: cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE
+      }),
+      removalPolicy: cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE
+    });
 
-        const handler = new lambdaNodejs.NodejsFunction(this, 'tls-generator-handler', {
-            entry: path.join(__dirname, '../lambdas/certificateGenerator.ts'),
-            handler: 'onEventHandler',
-            runtime: lambda.Runtime.NODEJS_LATEST,
-            timeout: cdk.Duration.minutes(15),
-            description: 'Generates TLS certificates for Druid internal components',
-        });
+    const handler = new lambdaNodejs.NodejsFunction(
+      this,
+      'tls-generator-handler',
+      {
+        entry: path.join(__dirname, '../lambdas/certificateGenerator.ts'),
+        handler: 'onEventHandler',
+        runtime: lambda.Runtime.NODEJS_LATEST,
+        timeout: cdk.Duration.minutes(15),
+        description: 'Generates TLS certificates for Druid internal components'
+      }
+    );
 
-        this.TlsCertificate.grantWrite(handler);
+    this.TlsCertificate.grantWrite(handler);
 
-        const provider = new cr.Provider(this, 'provider', { onEventHandler: handler });
+    const provider = new cr.Provider(this, 'provider', {
+      onEventHandler: handler
+    });
 
-        new cdk.CustomResource(this, 'tls-generator-custom-resource', {
-            serviceToken: provider.serviceToken,
-            properties: { TLSSecretId: this.TlsCertificate.secretArn },
-        });
-    }
+    new cdk.CustomResource(this, 'tls-generator-custom-resource', {
+      serviceToken: provider.serviceToken,
+      properties: { TLSSecretId: this.TlsCertificate.secretArn }
+    });
+  }
 }
